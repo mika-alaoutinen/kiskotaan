8b149eaf1ef67c1129b9da54711ecf73b8f3ead2
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-06-29 17:47:56 +0300

    Get players in App on component load

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import Players from './components/Player/Players'
+import { getPlayers } from './store/players/playerActions'
 
 const App: React.FC = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPlayers())
+  }, [dispatch])
   
   return (
     <div className="App">
--- src/store/players/playerReducer.ts
+++ src/store/players/playerReducer.ts
@@ -1,30 +1,23 @@
-import {
-  ADD_PLAYER, DELETE_PLAYER, GET_PLAYERS, PlayerActionTypes, PlayerState }
-  from './playerTypes'
+import { ADD_PLAYER, DELETE_PLAYER, GET_PLAYERS, PlayerActionTypes, PlayerState } from './playerTypes'
 
 const initialState: PlayerState = {
   players: []
 }
 
-// The reducer
 const playerReducer = (state = initialState, action: PlayerActionTypes): PlayerState => {
   switch (action.type) {
-
     case ADD_PLAYER:
       return {
         players: [ ...state.players, action.player ]
       }
-
     case DELETE_PLAYER:
       return {
         players: state.players.filter(player => player.id !== action.id)
       }
-
     case GET_PLAYERS:
       return {
         players: action.players
       }
-
     default:
       return state
   }
