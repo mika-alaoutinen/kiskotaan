2ff937ad30981121aed5c4a593cf039fc1c4be74
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-04 14:32:07 +0300

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,37 @@
-// Course information
 export interface Course {
   name: string,
   holes: Hole[],
   par: number
 }
 
-export interface Hole {
-  number: number,
-  par: number,
-  length?: number
-}
-
-// Player's information
 export interface Player {
   id: string,
-  name: string,
-  scoreCard: ScoreCard
+  name: string
 }
 
 export interface ScoreCard {
-  holes: Hole[]
+  id: string,
+  date?: Date,
+  holes: Hole[],
+  par: number,
+  players: Player[],
+  rows: ScoreCardRow[]
+}
+
+export interface ScoreCardRow {
+  scoreCardId: string,
+  holeNumber: number,
+  scores: PlayerScore[]
+}
+
+// Private interfaces
+interface Hole {
+  number: number,
+  par: number,
+  distance?: number
+}
+
+interface PlayerScore {
+  playerId: string,
+  score: number
 }
