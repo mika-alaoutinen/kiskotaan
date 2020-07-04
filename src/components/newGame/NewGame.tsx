006477583baf2d92011a998c750d521c7ca3774f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-04 15:03:34 +0300

    Add new stub components

---
+++ src/components/newGame/NewGame.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NewGame: React.FC = () => {
+
+  return (
+    <div>
+      <p>1. Valitse rata</p>
+      <p>2. Pelaajat</p>
+      <p>3. Aloita peli</p>
+    </div>
+  )
+}
+
+export default NewGame
