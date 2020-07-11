9bd81b42be44a7bb765433cf56e40b02f5d40bf0
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-11 10:16:04 +0300

    Add basic styles

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -4,35 +4,30 @@ import { Link } from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
-import { createStyles, makeStyles } from '@material-ui/core/styles'
 
 import Routes from '../../router/Routes'
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    appBar: {
-      top: 'auto',
-      bottom: 0,
-    },
-  }),
-)
-
-const Navigation: React.FC = () => {
-  const classes = useStyles()
-
-  return (
-    <div className='navigation'>
-      <Routes />
-
-      <AppBar className={classes.appBar} position='fixed'>
-        <ButtonGroup variant='text'>
-          <Button component={Link} to='/'>Home</Button>
-          <Button component={Link} to='/new-game'>New game</Button>
-          <Button component={Link} to='/history'>Game history</Button>
-        </ButtonGroup>
-      </AppBar>
-    </div>
-  )
+const appBarStyle = {
+  top: 'auto',
+  bottom: 0,
 }
+
+const centeredStyle = {
+  margin: '0 auto',
+}
+
+const Navigation: React.FC = () => (
+  <div className='navigation' >
+    <Routes />
+
+    <AppBar position='fixed' style={appBarStyle}>
+      <ButtonGroup variant='text' style={centeredStyle}>
+        <Button component={Link} to='/'>Home</Button>
+        <Button component={Link} to='/new-game'>New game</Button>
+        <Button component={Link} to='/history'>Game history</Button>
+      </ButtonGroup>
+    </AppBar>
+  </div>
+)
 
 export default Navigation
