68b97be4ec2299214d43a2021c1788733956895b
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-15 17:41:41 +0300

    Add courseActions

---
+++ src/store/courses/courseActions.ts
@@ -0,0 +1,26 @@
+import { AppThunk } from '../reduxTypes'
+import { ADD_COURSE, GET_COURSES } from './courseTypes'
+import { Course } from '../../types'
+import courseService from '../../services/courseService'
+
+export const addCourse = (course: Course): AppThunk => async dispatch => {
+  const newCourse: Course|void = await courseService.addCourse(course)
+
+  if (!newCourse) {
+    return
+  }
+
+  dispatch({
+    type: ADD_COURSE,
+    course: newCourse
+  })
+}
+
+export const getCourses = (): AppThunk => async dispatch => {
+  const courses: Course[]|void = await courseService.getCourses()
+
+  dispatch({
+    type: GET_COURSES,
+    courses
+  })
+}
