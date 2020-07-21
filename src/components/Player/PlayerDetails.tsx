02ceacdffd1438b42032eea0aec006698f37cf01
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-21 15:58:27 +0300

    WIP Players component

--- src/components/Player/PlayerDetails.tsx
+++ src/components/Player/PlayerDetails.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { Player } from '../../types'
 
 const PlayerDetails: React.FC<{ player: Player }> = ({ player }) => {
-  const { id, name } = player
+  const { name } = player
 
   return (
     <div>
-      <p>player id: {id}</p>
-      <p>player name: {name}</p>
+      <p>{name}</p>
     </div>
   )
 }
--- src/components/Player/Players.tsx
+++ src/components/Player/Players.tsx
@@ -1,23 +1,63 @@
 import React from 'react'
 
-import PlayerDetails from './PlayerDetails'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListSubheader from '@material-ui/core/ListSubheader'
+import Add from '@material-ui/icons/Add'
+import Remove from '@material-ui/icons/Remove'
+
+// import PlayerDetails from './PlayerDetails'
 import { Player } from '../../types'
 import { useSelector } from '../../store/reduxTypes'
 
 const Players: React.FC = () => {
   const players: Player[] = useSelector(state => state.players)
+  const selectedPlayers: Player[] = useSelector(state => state.scoreCard.players)
 
-  const renderPlayers = () => players
-    ? players.map(player =>
-      <PlayerDetails key={player.id} player={player} />)
-    : <p>no players</p>
+  const renderAllPlayers = () =>
+    players.map(player => createListItem(player, <Add />))
 
+  const renderSelectedPlayers = () => selectedPlayers.length > 0
+    ? selectedPlayers.map(player => createListItem(player, <Remove />))
+    : <p>No selected players</p>
+
+  const createListItem = (player: Player, icon: JSX.Element) =>
+    <ListItem key={player.id} button>
+      <ListItemIcon>{icon}</ListItemIcon>
+      <ListItemText primary={player.name} />
+    </ListItem>
+  
   return (
     <div>
-      <p>player listing</p>
-      <div>{renderPlayers()}</div>
+      <List subheader={
+        <ListSubheader>Selected players</ListSubheader>
+      }>
+        {renderSelectedPlayers()}
+      </List>
+
+      <Divider />
+
+      <List subheader={
+        <ListSubheader>Players</ListSubheader>
+      }>
+        {renderAllPlayers()}
+      </List>
     </div>
   )
+
+  // const renderPlayers = () => players
+  //   ? players.map(player =>
+  //     <PlayerDetails key={player.id} player={player} />)
+  //   : <p>no players</p>
+
+  // return (
+  //   <div>
+  //     {renderPlayers()}
+  //   </div>
+  // )
 }
 
 export default Players
