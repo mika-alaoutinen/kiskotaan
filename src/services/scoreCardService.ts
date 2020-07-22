8eebdb29bcfdaded7ec9f4bf79ea2e451f1badb4
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-22 16:50:57 +0300

    Add mock function in score card service

--- src/services/scoreCardService.ts
+++ src/services/scoreCardService.ts
@@ -1,24 +1,55 @@
 import axios from 'axios'
-import { scoresUrl } from '../constants'
+import { scoreCardsUrl } from '../constants'
 import { NewScoreCard, ScoreCard, ScoreCardRow } from '../types'
 
 const getScoreCard = async (): Promise<ScoreCard|void> =>
-  axios.get<ScoreCard>(scoresUrl)
+  axios.get<ScoreCard>(scoreCardsUrl)
     .then(response => response.data)
     .catch(error => console.log(error))
 
-const createScoreCard = async (newScoreCard: NewScoreCard): Promise<ScoreCard|void> =>
-  axios.post<ScoreCard>(scoresUrl, newScoreCard)
-    .then(response => response.data)
-    .catch(error => console.log(error))
+// const createScoreCard = async (newScoreCard: NewScoreCard): Promise<ScoreCard|void> =>
+//   axios.post<ScoreCard>(scoreCardUrl, newScoreCard)
+//     .then(response => response.data)
+//     .catch(error => console.log(error))
+
+const createScoreCard = async (newScoreCard: NewScoreCard): Promise<ScoreCard|void> => {
+  console.log('service', newScoreCard)
+
+  const scoreCard = {
+    id: 'test',
+    course: {
+      id: 'test',
+      name: 'test course',
+      holes: [
+        {
+          number: 1,
+          par: 3,
+          distance: 100
+        }
+      ],
+      par: 3
+    },
+    players: [
+      {
+        id: 'test',
+        name: 'Pekka'
+      }
+    ],
+    rows: [
+
+    ],
+  }
+
+  return Promise.resolve(scoreCard)
+}
 
 const addScore = async (score: ScoreCardRow): Promise<ScoreCard|void> =>
-  axios.post<ScoreCard>(scoresUrl, score)
+  axios.post<ScoreCard>(scoreCardsUrl, score)
     .then(response => response.data)
     .catch(error => console.log(error))
 
 const updateScore = async (score: ScoreCardRow): Promise<ScoreCard|void> =>
-  axios.put<ScoreCard>(scoresUrl, score)
+  axios.put<ScoreCard>(scoreCardsUrl, score)
     .then(response => response.data)
     .catch(error => console.log(error))
 
