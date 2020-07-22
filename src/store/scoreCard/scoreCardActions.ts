77a65d795ad3c78b804abb3aaa688511d0d5e29f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-22 11:56:59 +0300

    Add action for persisting new score card to backend

--- src/store/scoreCard/scoreCardActions.ts
+++ src/store/scoreCard/scoreCardActions.ts
@@ -1,20 +1,35 @@
 import {
-  ADD_PLAYER_TO_CARD, REMOVE_PLAYER_FROM_CARD, SELECT_COURSE, ScoreCardAction
+  ADD_PLAYER_TO_CARD, CREATE_SCORECARD, REMOVE_PLAYER_FROM_CARD, SELECT_COURSE, ScoreCardAction
 } from '../scoreCard/scoreCardTypes'
 
-import { Course, Player } from '../../types'
+import { AppThunk } from '../reduxTypes'
+import { Course, NewScoreCard, Player, ScoreCard } from '../../types'
+import scoreCardService from '../../services/scoreCardService'
 
 export const selectCourse = (course: Course): ScoreCardAction => ({
   type: SELECT_COURSE,
   course
 })
 
-export const addPlayer = (player: Player): ScoreCardAction => ({
+export const addPlayerToCard = (player: Player): ScoreCardAction => ({
   type: ADD_PLAYER_TO_CARD,
   player
 })
 
-export const removePlayer = (player: Player): ScoreCardAction => ({
+export const removePlayerFromCard = (player: Player): ScoreCardAction => ({
   type: REMOVE_PLAYER_FROM_CARD,
   player
 })
+
+/**
+ * Persists the new score card to backend once it has been filled in.
+ * @param newScoreCard scoreCard without ID.
+ */
+export const createNewScoreCard = (newScoreCard: NewScoreCard): AppThunk => async dispatch => {
+  const scoreCard: ScoreCard|void = await scoreCardService.createScoreCard(newScoreCard)
+
+  dispatch({
+    type: CREATE_SCORECARD,
+    scoreCard
+  })
+}
--- src/store/scoreCard/scoreCardTypes.ts
+++ src/store/scoreCard/scoreCardTypes.ts
@@ -4,10 +4,15 @@ import { Course, Player, ScoreCard } from '../../types'
 export const SELECT_COURSE = 'SELECT_COURSE'
 export const ADD_PLAYER_TO_CARD = 'ADD_PLAYER_TO_CARD'
 export const REMOVE_PLAYER_FROM_CARD = 'REMOVE_PLAYER_FROM_CARD'
+export const CREATE_SCORECARD = 'CREATE_SCORECARD'
 
 // Types for state and actions:
 export type ScoreCardState = ScoreCard
-export type ScoreCardAction = SelectCourseAction | AddPlayerAction | RemovePlayerAction
+export type ScoreCardAction =
+  | SelectCourseAction
+  | AddPlayerAction
+  | RemovePlayerAction
+  | CreateScoreCardAction
 
 interface SelectCourseAction {
   type: typeof SELECT_COURSE,
@@ -22,4 +27,9 @@ interface AddPlayerAction {
 interface RemovePlayerAction {
   type: typeof REMOVE_PLAYER_FROM_CARD,
   player: Player
+}
+
+interface CreateScoreCardAction {
+  type: typeof CREATE_SCORECARD,
+  scorecard: ScoreCard
 }
