8c3c81e4ea324a0da8d564152a6e4e1c3a10a4ce
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-23 11:07:28 +0300

    Basic implementation for NewGameSummary

--- src/components/newGame/NewGameSummary.tsx
+++ src/components/newGame/NewGameSummary.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import CourseDetails from '../course/CourseDetails'
+import PlayerDetails from '../player/PlayerDetails'
+import { Course, Player } from '../../types'
+import { useSelector } from '../../store/reduxTypes'
+
 const NewGameSummary: React.FC = () => {
+  const course: Course = useSelector(state => state.newScoreCard.course)
+  const players: Player[] = useSelector(state => state.newScoreCard.players)
+  
+  const renderPlayers = (): JSX.Element[] =>
+    players.map(player =>
+      <PlayerDetails key={player.id} player={player} />)
+  
   return (
     <div>
-      Start a new game. Show summary of course and players here.
+      <div className='selected-course'>
+        <CourseDetails course={course} />
+      </div>
+
+      <div className='selected-players'>
+        <h3>Players</h3>
+        {renderPlayers()}
+      </div>
     </div>
   )
 }
