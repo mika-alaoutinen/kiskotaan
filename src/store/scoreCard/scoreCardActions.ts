21c920fece7850887e1a351bd45ed4f7afb1dafc
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-23 12:16:36 +0300

    Add action for getting a score card

--- src/store/scoreCard/scoreCardActions.ts
+++ src/store/scoreCard/scoreCardActions.ts
@@ -1,11 +1,12 @@
 import scoreCardService from '../../services/scoreCardService'
 import { AppThunk } from '../reduxTypes'
-import { CREATE_SCORECARD } from './scoreCardTypes'
+import { CREATE_SCORECARD, GET_SCORECARD } from './scoreCardTypes'
 import { RESET_CARD } from '../newScoreCard/newScoreCardTypes'
 import { NewScoreCard, ScoreCard } from '../../types'
 
 /**
  * Persists the new score card to backend once it has been filled in.
+ * Resets the state of temporary newScoreCard.
  */
 export const createNewScoreCard = (): AppThunk => async (dispatch, getState) => {
   const newScoreCard: NewScoreCard = getState().newScoreCard
@@ -18,5 +19,14 @@ export const createNewScoreCard = (): AppThunk => async (dispatch, getState) => 
 
   dispatch({
     type: RESET_CARD
+  })
+}
+
+export const getScoreCard = (id: string): AppThunk => async dispatch => {
+  const scoreCard: ScoreCard|void = await scoreCardService.getScoreCard(id)
+
+  dispatch({
+    type: GET_SCORECARD,
+    scoreCard
   })
 }
