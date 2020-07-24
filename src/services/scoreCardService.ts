61e5ff4e42ef5b246340317d6d1ce3b25da3fe8f
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-24 19:26:08 +0300

    Return mock response from score card service

--- src/services/scoreCardService.ts
+++ src/services/scoreCardService.ts
@@ -2,15 +2,24 @@ import axios, { AxiosResponse } from 'axios'
 import { scoreCardsUrl } from '../constants'
 import { NewScoreCard, ScoreCard } from '../types'
 
+// Return mock response with ScoreRows:
+import response from '../response.json'
+
 const getScoreCard = async (id: string): Promise<ScoreCard|void> =>
-  axios.get<ScoreCard>(scoreCardsUrl + '/' + id)
-    .then(response => response.data)
-    .catch(error => console.log(error))
+  Promise.resolve(response)
 
 const createScoreCard = async (newScoreCard: NewScoreCard): Promise<ScoreCard|void> =>
-  axios.post<ScoreCard>(scoreCardsUrl, newScoreCard)
-    .then(response => response.data)
-    .catch(error => console.log(error))
+  Promise.resolve(response)
+
+// const getScoreCard = async (id: string): Promise<ScoreCard|void> =>
+//   axios.get<ScoreCard>(scoreCardsUrl + '/' + id)
+//     .then(response => response.data)
+//     .catch(error => console.log(error))
+
+// const createScoreCard = async (newScoreCard: NewScoreCard): Promise<ScoreCard|void> =>
+//   axios.post<ScoreCard>(scoreCardsUrl, newScoreCard)
+//     .then(response => response.data)
+//     .catch(error => console.log(error))
 
 const deleteScoreCard = async (id: string): Promise<void|AxiosResponse<void>> =>
   axios.delete<void>(scoreCardsUrl + '/' + id)
