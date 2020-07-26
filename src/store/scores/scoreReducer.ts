e703883e4495016fd338460a995878f2658bb8bb
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-26 14:35:27 +0300

    WIP implement substract score in reducer

--- src/store/scores/scoreReducer.ts
+++ src/store/scores/scoreReducer.ts
@@ -1,18 +1,53 @@
-import { UPDATE_SCORES, ScoreAction, ScoreState } from './scoreTypes'
+import {
+  SUBSTRACT_SCORE, UPDATE_SCORES,
+  ScoreAction, ScoreState
+} from './scoreTypes'
+import { ScoreRow, Score } from '../../types'
 
 const scoreReducer = (state: ScoreState = [], action: ScoreAction): ScoreState => {
   switch (action.type) {
 
+    case SUBSTRACT_SCORE:
+      const { playerId, hole, currentScore } = action
+      return substract(state, playerId, hole, currentScore)
+    
     case UPDATE_SCORES:
-      const { hole, scores } = action.row
       return [
         ...state,
-        { hole, scores }
+        {
+          hole: action.row.hole,
+          scores: action.row.scores
+        }
       ]
-
+      
     default:
       return state
   }
+}
+
+const substract = (
+  state: ScoreState, playerId: string, hole: number, newScore: number
+): ScoreState => {
+
+  const updatedScore: Score = {
+    playerId,
+    score: newScore
+  }
+  
+  const row: ScoreRow|undefined = state.find(row => row.hole === hole)
+  if (!row) {
+    return state
+  }
+
+  const updatedScores:Score[] = row.scores.map(score => score.playerId === playerId ? updatedScore : score)
+  
+  return [
+    ...state,
+    {
+      hole,
+      scores: updatedScores
+    }
+  ]
 }
 
 export default scoreReducer
