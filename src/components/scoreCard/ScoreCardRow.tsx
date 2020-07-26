db1a7e0131a6e244d7d763ca557a04457a32e6ab
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-26 16:44:21 +0300

    WIP ScoreCardRow

--- src/components/scoreCard/ScoreCardRow.tsx
+++ src/components/scoreCard/ScoreCardRow.tsx
@@ -1,26 +1,34 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 import Add from '@material-ui/icons/Add'
+import Button from '@material-ui/core/Button'
 import Chip from '@material-ui/core/Chip'
 import IconButton from '@material-ui/core/IconButton'
 import Remove from '@material-ui/icons/Remove'
 
-import { Player, ScoreCard } from '../../types'
+import { substractScore, updateScores } from '../../store/scores/scoreActions'
+import { Player, ScoreCard, ScoreRow } from '../../types'
 import { useSelector } from '../../store/reduxTypes'
 
 const ScoreCardRow: React.FC<{ par: number, player: Player}> = ({ par, player }) => {
-  const hole: number = useSelector(state => state.game.hole)
-  const scoreCard: ScoreCard = useSelector(state => state.scoreCard)
-
+  const dispatch = useDispatch()
   const [ score, setScore ] = useState(par)
+  
+  const hole: number = useSelector(state => state.game.hole)
+  const scoreRows: ScoreRow[] = useSelector(state => state.scoreCard.rows)
 
   const getScore = (): number => {
-    const playerScore: number|undefined = scoreCard.rows
+    const playerScore: number|undefined = scoreRows
       .find(row => row.hole === hole)
       ?.scores.find(score => score.playerId === player.id)
       ?.score
 
     return playerScore ? playerScore : par
+  }
+
+  const minusHandler = (): void => {
+    dispatch(substractScore(player.id, hole, getScore() - 1))
   }
 
   const chipColor = (): string => {
@@ -43,14 +51,14 @@ const ScoreCardRow: React.FC<{ par: number, player: Player}> = ({ par, player })
       <IconButton
         color='primary'
         disabled={score === 1}
-        onClick={() => setScore(score - 1)}
+        onClick={minusHandler}
       >
         <Remove />
       </IconButton>
 
       <Chip
         color='secondary'
-        label={score}
+        label={getScore()}
         style={{ backgroundColor: chipColor() }}
       />
 
@@ -60,7 +68,10 @@ const ScoreCardRow: React.FC<{ par: number, player: Player}> = ({ par, player })
       >
         <Add />
       </IconButton>
-      <button onClick={() => console.log(scoreCard)}>log</button>
+
+      {/* <Button onClick={() => dispatch(updateScores(hole))}>
+        Save
+      </Button> */}
     </div>
   )
 }
