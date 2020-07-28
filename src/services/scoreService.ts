ff99213fa39bc2d222c937817dabcf2d443259df
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-28 17:48:04 +0300

    Update score service

--- src/services/scoreService.ts
+++ src/services/scoreService.ts
@@ -1,15 +1,14 @@
 import axios from 'axios'
 import { scoreCardsUrl } from '../constants'
-import { ScoreCard, ScoreRow } from '../types'
+import { ScoreRow } from '../types'
 
 const addScore = async (score: ScoreRow): Promise<ScoreRow|void> =>
   axios.post<ScoreRow>(scoreCardsUrl, score)
     .then(response => response.data)
     .catch(error => console.log(error))
 
-// Not needed? Do you really want to return ScoreCard?
-const updateScore = async (score: ScoreRow): Promise<ScoreCard|void> =>
-  axios.put<ScoreCard>(scoreCardsUrl, score)
+const updateScore = async (score: ScoreRow): Promise<ScoreRow|void> =>
+  axios.put<ScoreRow>(scoreCardsUrl, score)
     .then(response => response.data)
     .catch(error => console.log(error))
 
