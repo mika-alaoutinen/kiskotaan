eea5448882c067caf83be13665372fee6404a2ec
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-28 18:33:39 +0300

    Delete redundant addScore function from score service

--- src/services/scoreService.ts
+++ src/services/scoreService.ts
@@ -2,14 +2,9 @@ import axios from 'axios'
 import { scoreCardsUrl } from '../constants'
 import { ScoreRow } from '../types'
 
-const addScore = async (score: ScoreRow): Promise<ScoreRow|void> =>
-  axios.post<ScoreRow>(scoreCardsUrl, score)
-    .then(response => response.data)
-    .catch(error => console.log(error))
-
 const updateScore = async (id: string, score: ScoreRow): Promise<ScoreRow|void> =>
   axios.put<ScoreRow>(scoreCardsUrl + '/' + id, score)
     .then(response => response.data)
     .catch(error => console.log(error))
 
-export default { addScore, updateScore }
+export default { updateScore }
