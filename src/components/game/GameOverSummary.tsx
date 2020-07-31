e9954b75c78769d2cca90574194ad13ca873ee26
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-07-31 20:12:38 +0300

    Add redirect button to home to GameOverSummary

--- src/components/game/GameOverSummary.tsx
+++ src/components/game/GameOverSummary.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import RedirectButton from '../common/RedirectButton'
+import { homePath } from '../../constants'
+
 const GameOverSummary: React.FC = () => {
   return (
     <div>
       game over
+      <RedirectButton text='To home page' to={homePath} />
     </div>
   )
 }
