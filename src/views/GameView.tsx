1587312144aec85e06f4b9bbd5d630ac5df28a25
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-08 16:02:18 +0300

    Refactor game over view slightly

--- src/views/GameView.tsx
+++ src/views/GameView.tsx
@@ -3,20 +3,23 @@ import { useDispatch } from 'react-redux'
 import { useParams } from 'react-router-dom'
 
 import EndGameButton from '../components/game/EndGameButton'
-import GameOverSummary from '../components/game/GameOverSummary'
 import HoleSelectionButtons from '../components/game/HoleSelectionButtons'
 import HoleSelector from '../components/game/HoleSelector'
+import RedirectButton from '../components/common/RedirectButton'
 import ScoreCardDetails from '../components/scoreCard/ScoreCardDetails'
-import { Game, ScoreCard } from '../types'
+import ScoreTable from '../components/scoreTable/ScoreTable'
+import { homePath } from '../constants'
 import { getScoreCard } from '../store/scoreCard/scoreCardActions'
+import { Game, ScoreCard } from '../types'
 import { useSelector } from '../store/reduxTypes'
 
 const GameView: React.FC = () => {
   const { id } = useParams<{ id: string }>()
-
   const dispatch = useDispatch()
+  
   const game: Game = useSelector(state => state.game)
   const scoreCard: ScoreCard = game.scoreCard
+  const { name, holes } = scoreCard.course
   
   // If score card is not in store, get it from backend:
   useEffect(() => {
@@ -34,7 +37,17 @@ const GameView: React.FC = () => {
     </>
 
   const renderGameOverView = (): JSX.Element =>
-    <GameOverSummary />
+    <>
+      {renderGameOverHeading()}
+      <ScoreTable />
+      <RedirectButton text='To home page' to={homePath} />
+    </>
+  
+  const renderGameOverHeading = (): JSX.Element =>
+    <>
+      <h2>Game summary</h2>
+      <p>{name} {holes.length}, {game.date}</p>
+    </>
   
   return game.isOver ? renderGameOverView() : renderGameView()
 }
