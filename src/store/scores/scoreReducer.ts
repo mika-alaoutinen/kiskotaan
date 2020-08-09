18af07e8363300ac29b9b678d81def31c64f2b9d
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-09 19:08:55 +0300

    Refactor scoreReducer

--- src/store/scores/scoreReducer.ts
+++ src/store/scores/scoreReducer.ts
@@ -1,24 +1,41 @@
-import { CHANGE_SCORE, UPDATE_SCORES, ScoreAction, ScoreState } from './scoreTypes'
-import { ScoreRow } from '../../types'
+import { ADD_SCORE, SUBSTRACT_SCORE, UPDATE_SCORES, ScoreAction, ScoreState } from './scoreTypes'
+import { Player } from '../../types'
 
 const scoreReducer = (state: ScoreState = [], action: ScoreAction): ScoreState => {
   switch (action.type) {
     
-    case CHANGE_SCORE:
-      return changeScore(state, action.row, action.hole)
-    
+    case ADD_SCORE:
+      return addOrSubstract(state, action.playerId, action.hole, +1)
+
+    case SUBSTRACT_SCORE:
+      return addOrSubstract(state, action.playerId, action.hole, -1)
+
     case UPDATE_SCORES:
-      return changeScore(state, action.row, action.hole)
+      return state
             
     default:
       return state
   }
 }
 
-const changeScore = (
-  state: ScoreState, updatedRow: ScoreRow, hole: number
-): ScoreState =>
+const addOrSubstract = (
+  state: ScoreState, playerId: string, hole: number, scoreModifier: -1|1
+): ScoreState => {
 
-  state.map(row => row.hole === hole ? updatedRow : row)
+  const editedPlayer: Player|undefined = state.find(player => player.id === playerId)
+  if (!editedPlayer) {
+    return state
+  }
+
+  editedPlayer.scores.map(score => score.hole === hole
+    ? {
+      ...score,
+      score: score.score + scoreModifier
+    }
+    : score
+  )
+
+  return state.map(player => player.id === editedPlayer.id ? editedPlayer : player)
+}
 
 export default scoreReducer
