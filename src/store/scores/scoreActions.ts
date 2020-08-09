3f2346cda0d2ef0191bd0fbad031572e84f1ab58
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-09 19:08:45 +0300

    Refactor scoreActions

--- src/store/scores/scoreActions.ts
+++ src/store/scores/scoreActions.ts
@@ -1,88 +1,51 @@
 import scoreService from '../../services/scoreService'
 import { AppThunk } from '../reduxTypes'
-import { CHANGE_SCORE, UPDATE_SCORES, ChangeScore } from './scoreTypes'
-import { Score, ScoreCard, ScoreRow } from '../../types'
+import { ADD_SCORE, SUBSTRACT_SCORE, UPDATE_SCORES } from './scoreTypes'
 import { setScoreChanged } from '../game/gameActions'
+import { PlayerScore, Score } from '../../types'
 
-export const addScore = (playerId: string, hole: number): AppThunk =>
-  (dispatch, getState) => {
-    
-    const rows: ScoreRow[] = getState().game.scoreCard.rows
-    dispatch(setScoreChanged())
-    dispatch(addOrSubstractScore(rows, playerId, hole, 1))
-  }
+export const addScore = (playerId: string, hole: number): AppThunk => dispatch => {
+  dispatch({
+    type: ADD_SCORE,
+    playerId,
+    hole
+  })
 
-export const substractScore = (playerId: string, hole: number): AppThunk =>
-  (dispatch, getState) => {
-
-  const rows: ScoreRow[] = getState().game.scoreCard.rows
   dispatch(setScoreChanged())
-  dispatch(addOrSubstractScore(rows, playerId, hole, -1))
 }
 
-export const updateScores = (hole: number): AppThunk =>
-  async (dispatch, getState) => {
+export const substractScore = (playerId: string, hole: number): AppThunk => dispatch => {
+  dispatch({
+    type: SUBSTRACT_SCORE,
+    playerId,
+    hole
+  })
+  
+  dispatch(setScoreChanged())
+}
 
-  const scoreCard: ScoreCard = getState().game.scoreCard
-  const scoreRow: ScoreRow|undefined = scoreCard.rows.find(row => row.hole === hole)
-  const row = scoreRow
-    ? await scoreService.updateScore(scoreCard.id, scoreRow)
-    : undefined
+export const updateScores = (hole: number): AppThunk => async (dispatch, getState) => {
+  const { id, players } = getState().game.scoreCard
 
-  if (!row) {
+  const playerScores: PlayerScore[] = players.map(player => {
+    const score: Score|undefined = player.scores.find(score => score.hole === hole)
+    return {
+      playerId: player.id,
+      score: {
+        hole,
+        score: score ? score.score : -1
+      }
+    }
+  })
+
+  const updated: PlayerScore[]|void = await scoreService.updateScores(id, playerScores)
+
+  if (!updated) {
     return
   }
-
+  
   dispatch({
     type: UPDATE_SCORES,
-    hole,
-    row
+    scores: updated
   })
-}
-
-const addOrSubstractScore = (
-  rows: ScoreRow[], playerId: string, hole: number, scoreModifier: 1|-1
-): ChangeScore => {
-
-  const scores: Score[] = findScoresForHole(rows, hole)
-  const currentScore: number = findCurrentScore(rows, playerId, hole)
-  const updatedScore: Score = {
-    playerId,
-    score: currentScore + scoreModifier
-  }
-
-  return {
-    type: CHANGE_SCORE,
-    hole,
-    row: updateScoreRow(playerId, hole, scores, updatedScore)
-  }
-}
-
-const findScoresForHole = (rows: ScoreRow[], hole: number): Score[] => {
-  const row: ScoreRow|undefined = rows.find(row => row.hole === hole)
-  return row ? row.scores : []
-}
-
-const findCurrentScore = (
-  rows: ScoreRow[], playerId: string, hole: number
-): number => {
-
-  const score: number|undefined =  rows.find(row => row.hole === hole)
-    ?.scores.find(score => score.playerId === playerId)
-    ?.score
-  
-  return score ? score : 0
-}
-  
-const updateScoreRow = (
-  playerId: string, hole: number, scores: Score[], updatedScore: Score
-): ScoreRow => {
-  
-  const updatedScores: Score[] = scores.map(score =>
-    score.playerId === playerId ? updatedScore : score)
-  
-  return {
-    hole,
-    scores: updatedScores
-  }
 }
