eaebb1d66308161adf8c3da4bffe2a2a9177d788
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-09 19:41:01 +0300

    Edit newGame.json

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -2,28 +2,28 @@ import axios from 'axios'
 import { gamesUrl } from '../constants'
 import { Game } from '../types'
 
-// import game from '../newGame.json'
-
-// const startGame = async (scoreCard: ScoreCard): Promise<Game|void> =>
-//   Promise.resolve(game)
-
-// const endGame = async (id: string): Promise<Game|void> => {
-//   const endedGame: Game = {
-//     ...game,
-//     isOver: true
-//   }
-
-//   return Promise.resolve(endedGame)
-// }
+import game from '../newGame.json'
 
 const startGame = async (scoreCardId: string): Promise<Game|void> =>
-  axios.post<Game>(gamesUrl, scoreCardId)
-    .then(response => response.data)
-    .catch(error => console.log(error))
+  Promise.resolve(game)
 
-const endGame = async (gameId: string): Promise<Game|void> =>
-  axios.post<Game>(gamesUrl, gameId)
-  .then(response => response.data)
-  .catch(error => console.log(error))
+const endGame = async (gameId: string): Promise<Game|void> => {
+  const endedGame: Game = {
+    ...game,
+    isOver: true
+  }
+
+  return Promise.resolve(endedGame)
+}
+
+// const startGame = async (scoreCardId: string): Promise<Game|void> =>
+//   axios.post<Game>(gamesUrl, scoreCardId)
+//     .then(response => response.data)
+//     .catch(error => console.log(error))
+
+// const endGame = async (gameId: string): Promise<Game|void> =>
+//   axios.post<Game>(gamesUrl, gameId)
+//   .then(response => response.data)
+//   .catch(error => console.log(error))
 
 export default { startGame, endGame }
