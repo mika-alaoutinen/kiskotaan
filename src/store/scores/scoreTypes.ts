8d0fd895ca7b289a140a0a40ee3945539ff1d6bf
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-09 19:08:25 +0300

    Refactor scoreTypes

--- src/store/scores/scoreTypes.ts
+++ src/store/scores/scoreTypes.ts
@@ -1,21 +1,27 @@
-import { ScoreRow } from '../../types'
+import { Player, PlayerScore } from '../../types'
 
 // Action names:
-export const CHANGE_SCORE = 'CHANGE_SCORE'
+export const ADD_SCORE = 'ADD_SCORE'
+export const SUBSTRACT_SCORE = 'SUBSTRACT_SCORE'
 export const UPDATE_SCORES = 'UPDATE_SCORES'
 
 // Types for state and actions:
-export type ScoreState = ScoreRow[]
-export type ScoreAction = ChangeScore | UpdateScores
+export type ScoreState = Player[]
+export type ScoreAction = AddScore | SubstractScore | UpdateScores
 
-export interface ChangeScore {
-  type: typeof CHANGE_SCORE,
-  hole: number,
-  row: ScoreRow
+export interface AddScore {
+  type: typeof ADD_SCORE,
+  playerId: string,
+  hole: number
 }
 
-interface UpdateScores {
+export interface SubstractScore {
+  type: typeof SUBSTRACT_SCORE,
+  playerId: string,
+  hole: number
+}
+
+export interface UpdateScores {
   type: typeof UPDATE_SCORES,
-  hole: number,
-  row: ScoreRow
+  scores: PlayerScore[]
 }
