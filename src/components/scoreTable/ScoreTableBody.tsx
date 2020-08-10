a13992364a51c3baaad01a7f983a5b149b1887ba
Mika Alaoutinen <mika.alaoutinen@gmail.com>
2020-08-10 19:29:16 +0300

    Map ResultRows in ScoreTableBody

--- src/components/scoreTable/ScoreTableBody.tsx
+++ src/components/scoreTable/ScoreTableBody.tsx
@@ -4,28 +4,48 @@ import {
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper
 } from '@material-ui/core'
 
-import { ScoreCard } from '../../types'
+import { ScoreCard, ScoreRow } from '../../types'
 import { useScoreCard } from '../../hooks/scoreCardHooks'
+
+interface ResultRow {
+  hole: number,
+  par: number,
+  scores: number[]
+}
 
 const ScoreTableBody: React.FC<{ scoreCardId: string }> = ({ scoreCardId }) => {
   const scoreCard: ScoreCard = useScoreCard(scoreCardId)
 
+  const resultRows: ResultRow[] = []
+
+  for (const hole of scoreCard.course.holes) {
+    const scoreRow: ScoreRow|undefined = scoreCard.rows.find(row => row.hole === hole.number)
+
+    const resultRow: ResultRow = {
+      hole: hole.number,
+      par: hole.par,
+      scores: scoreRow ? scoreRow.scores.map(score => score.score) : []
+    }
+
+    resultRows.push(resultRow)
+  }
+
   const renderRows = (): JSX.Element[] =>
-    scoreCard.course.holes.map(hole =>
-      <TableRow key={hole.number}>
-        <TableCell>{hole.number}</TableCell>
-        <TableCell>{hole.par}</TableCell>
-        {renderScores()}
+    resultRows.map(row =>
+      <TableRow key={row.hole}>
+        <TableCell>{row.hole}</TableCell>
+        <TableCell>{row.par}</TableCell>
+        {renderScores(row)}
       </TableRow>
     )
-
-  const renderScores = (): JSX.Element[] =>
-    scoreCard.rows.flatMap(row =>
-      row.scores.map(score =>
-        <TableCell key={score.playerId}>{score.score}</TableCell>
-      )
+  
+  const renderScores = (row: ResultRow): JSX.Element[] =>
+    row.scores.map(score =>
+      <TableCell key={score}>
+        {score}
+      </TableCell>
     )
-
+  
   return (
     <TableContainer component={Paper}>
       <Table>
